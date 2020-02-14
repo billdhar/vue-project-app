4a542f858e12cf3ab5d4187763fde031daafbcee
billdhar <billdharx@gmail.com>
2020-02-15 04:42:13 +0530

    Added SSL Certificate

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const { GenerateSW } = require('workbox-webpack-plugin')
+const fs = require('fs')
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'development' ? '/vuejs-pwa/' : '',
+  configureWebpack: {
+    plugins: [new GenerateSW()]
+  },
+  devServer: {
+    https: {
+      key: fs.readFileSync('./certs/key.pem'),
+      cert: fs.readFileSync('./certs/cert.pem')
+    }
+  }
+}
