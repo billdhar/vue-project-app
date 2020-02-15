60d4e7428ff69cbfb02bbdd2251ee0dfa8719182
billdhar <billdharx@gmail.com>
2020-02-15 05:46:40 +0530

    IP for GCP Added

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,6 @@ module.exports = {
       key: fs.readFileSync('./certs/key.pem'),
       cert: fs.readFileSync('./certs/cert.pem')
     },
-    public: 'https://172.17.0.2:8080'
+    public: 'https://35.204.132.106:8080'
   }
 }
