9a1694e14289505ad1fd9bb2fc2c0bb3a22db7ae
billdhar <billdharx@gmail.com>
2020-02-15 08:39:02 +0530

    Commit Before Break

---
+++ src/views/Town.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="town">
+    <img alt="Nope! :)" src="../assets/nope.gif" width=350>
+    <h1 class="bigText">You got THAT right!</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.bigText {
+  font-size: 50pt;
+}
+</style>
