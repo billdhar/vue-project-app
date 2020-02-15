004c9c0fadb4f5f472fa37f418e3528779d72d03
billdhar <billdharx@gmail.com>
2020-02-15 12:57:27 +0530

    Camera Addition

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import CameraView from '@/components/CameraView'
 
 Vue.use(VueRouter)
 
@@ -25,6 +26,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Town.vue')
+  },
+  {
+    path: '/camera',
+    name: 'camera',
+    component: CameraView
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Fuk U Gonna Do? Fite Me?" src="../assets/logo.png" width=350>
+    <img alt="Fuk U Gonna Do? Fite Me?" src="../assets/logo.png" width=350/>
     <h1 class="bigText">Huh? Huh?</h1>
+    <div class="press">
+    <router-link class="take-picture-button mdl-button mdl-js-button mdl-button--fab mdl-button--colored" to="/camera">
+      <img class="press" alt="camera" src="../assets/camera.jpg" width=128/>
+    </router-link>
+  </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .bigText {
   font-size: 50pt;
+}
+.press {
+  margin-top: 20px;
 }
 </style>
