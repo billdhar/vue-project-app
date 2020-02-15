18da7985ee2a37c1645e1f9906b2fd042de38bd2
billdhar <billdharx@gmail.com>
2020-02-15 08:18:42 +0530

    After Page Addition

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
+      <router-link to="/about">Aw Damn!</router-link> |
       <router-link to="/">Fite Me?</router-link> |
-      <router-link to="/about">Aw Damn!</router-link>
+      <router-link to="/town">Nope</router-link>
     </div>
     <router-view/>
   </div>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import wb from './registerServiceWorker'
+
 Vue.prototype.$workbox = wb
 
 Vue.config.productionTip = false
